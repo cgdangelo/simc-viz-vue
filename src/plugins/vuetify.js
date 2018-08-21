81c79eee204c7d57301ed2cecc8fe7329be17659
Charles D'Angelo <c.g.dangelo@gmail.com>
2018-08-21 08:59:58 -0400

    Remove boilerplate

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,17 +1,9 @@
 import Vue from 'vue'
-import { transitions, VApp, VBtn, VFooter, VGrid, VIcon, VList, VNavigationDrawer, VToolbar, Vuetify } from 'vuetify'
+import { VApp, Vuetify } from 'vuetify'
 import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
   components: {
-    VApp,
-    VNavigationDrawer,
-    VFooter,
-    VList,
-    VBtn,
-    VIcon,
-    VGrid,
-    VToolbar,
-    transitions
+    VApp
   }
 })
