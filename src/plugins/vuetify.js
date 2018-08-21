796461192619f470e27abfc67553888bce70267f
Charles D'Angelo <c.g.dangelo@gmail.com>
2018-08-21 08:57:54 -0400

    Add vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import { transitions, VApp, VBtn, VFooter, VGrid, VIcon, VList, VNavigationDrawer, VToolbar, Vuetify } from 'vuetify'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  }
+})
